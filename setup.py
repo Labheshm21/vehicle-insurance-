436d6697e78ab3844ed936e2e82fb1b3f102f363
Labhesh Mahajan <lmahajan@wpi.edu>
2026-06-16 17:11:47 -0400

    project structure ready and venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Labhesh Mahajan",
+    author_email="labheshmahajan02@gmail.com",
+    packages=find_packages()
+)
